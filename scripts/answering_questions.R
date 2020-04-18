4a8e84088d596da210396c68ee8b3b2922b7da1f
Perera <per24h@csiro.au>
2020-04-18 13:40:12 +1000

    end of question 2

--- scripts/answering_questions.R
+++ scripts/answering_questions.R
@@ -22,4 +22,22 @@ bom_data_temp_rf <- bom_data %>%
 
 bom_data_temp_rf 
 
+# question 2 - which month saw the lowest average temperature difference
+
+temp_diff <- bom_data %>% 
+  separate(Temp_min_max,into = c("t_min", "t_max"), sep="/")%>% 
+  filter(t_min != "-",t_max != "-") %>% 
+  mutate(t_min = as.numeric(t_min))%>% # need to convert values in these columns to numerics
+  mutate(t_max = as.numeric(t_max ))%>%
+  mutate(temp_difference = t_max-t_min) %>% # to calculate the temperature difference
+select(Station_number, Year, Month, Day, temp_difference) 
+
+month_lowest_temp_diff <- temp_diff %>% 
+  group_by(Month) %>% 
+  summarise(mean_temp_diff=mean(temp_difference))%>% # calculate the mean temperature difference
+  arrange(mean_temp_diff) 
+
+month_lowest_temp_diff
+
+# June was the month with lowest average daily temperature difference (8.74)
 
